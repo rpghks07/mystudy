91886dfc89e1b2f6f17fe145f7cbd1849f675c8c
rpghks07 <rpghks07@gmail.com>
2024-07-17 00:18:16 +0900

    2024-07-16

---
+++ CT/src/_202407/Main.java
@@ -0,0 +1,10 @@
+package _202407;
+
+public class Main {
+    public static void main(String[] args) {
+        ProgrammersBasic_String1 pb = new ProgrammersBasic_String1();
+
+        String result = pb.solution("aaaaa", "bbbbb");
+        System.out.println("result = " + result);
+    }
+}
---
+++ CT/src/_202407/ProgrammersBasic_String1.java
@@ -0,0 +1,12 @@
+package _202407;
+
+public class ProgrammersBasic_String1 {
+    public String solution(String str1, String str2) {
+
+        String answer = "";
+        for(int i = 0; i < str1.length(); i++){
+            answer += String.valueOf(str1.charAt(i)) + str2.charAt(i);
+        }
+        return answer;
+    }
+}
